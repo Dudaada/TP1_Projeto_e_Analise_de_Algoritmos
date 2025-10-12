5d3cc097f16dcaa914172f8c206d4c19f8bd5f27
Maria Eduarda Oliveira Garcia Andrade <132073613+Dudaada@users.noreply.github.com>
2025-10-12 11:50:24 -0300

    feat: alterei o main para que ele receba os arquivos em looping

--- src/main.c
+++ src/main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../include/entrada.h"
 
 int main() {
     char nomeArquivo[100];
-    printf("Digite o nome do arquivo de entrada (ex: testes/mapa1.txt): ");
-    scanf("%s", nomeArquivo);
+    int opcao = 1;
 
-    Mapa* m = lerArquivo(nomeArquivo);
-    if (m == NULL) return 1;
+    while (opcao != 0) {
+        printf("\n====================================\n");
+        printf("     EXPRESSO INTERESTELAR - PAA\n");
+        printf("=====================================\n");
+        printf("Nome do arquivo de entrada (ex: testes/mapa1.txt) ou digite '0' para sair: ");
+        scanf("%s", nomeArquivo);
 
-    imprimirMapa(m);
+        if (nomeArquivo[0] == '0') {
+            printf("\nEncerrando o programa...\n");
+            break;
+        }
 
-    liberarMapa(m);
+        Mapa* m = lerArquivo(nomeArquivo);
+        if (m == NULL) {
+            printf("\n Erro: nao foi possivel ler o arquivo '%s'.\n", nomeArquivo);
+            printf("Verifique o caminho e tente novamente.\n");
+            continue; // volta para o início do loop
+        }
+
+        imprimirMapa(m);
+
+        liberarMapa(m);
+
+        printf("\nDeseja carregar outro arquivo?\n");
+        printf("[1] Sim\n[0] Nao (sair)\n> ");
+        scanf("%d", &opcao);
+    }
+
     return 0;
 }
-
-//Primeiro escreve 'make', dps 'make run' kkkkkk
