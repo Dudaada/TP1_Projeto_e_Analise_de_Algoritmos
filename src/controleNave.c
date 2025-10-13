a30fef2cba480cb099383dccbc2fe3b416d5b0c2
Pamela Diniz <pamelalarad05@gmail.com>
2025-10-13 00:19:39 -0300

    fix: organizando e retirando umas redundancias

--- src/controleNave.c
+++ src/controleNave.c
@@ -15,7 +15,6 @@ int verificaMovimento(char direcao, Mapa* mapa,int x,int y,int* novoX,int* novoY
         case 'd':
             (*novoY)++;
             break;
-
         case 'c':
             (*novoX)--;
             break;
@@ -24,50 +23,50 @@ int verificaMovimento(char direcao, Mapa* mapa,int x,int y,int* novoX,int* novoY
             break;
     }
 
-    if (*novoX < 0 || *novoX >= mapa->altura || *novoY < 0 || *novoY >= mapa->largura || visitado[(*novoX)][(*novoY)])
+    if (*novoX < 0 || *novoX >= mapa->altura || *novoY < 0 || *novoY >= mapa->largura)
         return 0;
 
-    if (mapa->mapa[*novoX][*novoY] != '.')
-        return 1;
+    if (visitado[(*novoX)][(*novoY)]){
+        return 0;
+    }
 
-    return 0;
+    if (mapa->mapa[*novoX][*novoY] == '.')
+        return 0;
+
+    return 1;
 }
 
-int movimentarNave(Mapa* mapa, int** visitado, int** solucao,int x, int y, int xFim, int yFim) {
+int movimentarNave(Mapa* mapa, int** visitado, int** solucao,int x, int y) {
 
     printf("Linha: %d, Coluna: %d; D: %d, pecas restantes: %d\n",x,y,mapa->D,4-mapa->qtdPecas);
 
-    if (x == xFim && y == yFim) {
+    if (x == mapa->linhaFinal && y == mapa->colunaFinal) {
         solucao[x][y] = 1;
         return 1;
     }
 
-    if (x >= 0 && x < mapa->altura && y >= 0 && y < mapa->largura && mapa->mapa[x][y] != '.' && !visitado[x][y]) {
+    visitado[x][y] = 1;
+    solucao[x][y] = 1;
 
-        visitado[x][y] = 1;
-        solucao[x][y] = 1;
+    char direcoes[] = {'e','d','c','b'};
+    int novoX,novoY;
 
-        char direcoes[] = {'e','d','c','b'};
-        int novoX,novoY;
-
-        for (int i = 0; i < 4; i++) {
-
-            if (verificaMovimento(direcoes[i],mapa,x,y,&novoX,&novoY,visitado)){
-                if (movimentarNave(mapa, visitado, solucao, novoX, novoY, xFim, yFim)){
-                    return 1;
-                }
+    for (int i = 0; i < 4; i++) {
+        if (verificaMovimento(direcoes[i],mapa,x,y,&novoX,&novoY,visitado)){
+            if (movimentarNave(mapa, visitado, solucao, novoX, novoY)){
+                return 1;
             }
         }
-
-        solucao[x][y] = 0;
-        visitado[x][y] = 0;
     }
-    printf("rota invalida\n");
+    printf("backtracking em (%d,%d)\n",x,y);
+    solucao[x][y] = 0;
+    visitado[x][y] = 0;
+
     return 0;
 }
 
 void exibeCaminho(Mapa* mapa, int** solucao) {
-    printf("\nRota:\n");
+    printf("Rota:\n");
     for (int i = 0; i < mapa->altura; i++) {
         for (int j = 0; j < mapa->largura; j++) {
             if (solucao[i][j] == 1)
@@ -91,12 +90,12 @@ int encontraCaminho(Mapa* mapa) {
         solucao[i] = calloc(M, sizeof(int));
     }
 
-    if (movimentarNave(mapa, visitado, solucao, mapa->linhaInicial, mapa->colunaInicial, mapa->linhaFinal, mapa->colunaFinal)) {
+    int rotaEncontrada = movimentarNave(mapa, visitado, solucao, mapa->linhaInicial, mapa->colunaInicial);
+    if (rotaEncontrada) {
         exibeCaminho(mapa, solucao);
-        return 1;
+
     } else {
         printf("nenhum caminho encontrado");
-        return 0;
     }
 
     for (int i = 0; i < N; i++) {
@@ -105,4 +104,6 @@ int encontraCaminho(Mapa* mapa) {
     }
     free(visitado);
     free(solucao);
+
+    return rotaEncontrada;
 }
